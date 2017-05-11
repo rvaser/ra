d8052d029984b61978ce6bb68d3ad4f9dd31bb6c
rvaser <robert.vaser@fer.hr>
2017-05-11 17:54:36 +0200

    Updated ra

--- ra.py
+++ ra.py
@@ -9,12 +9,12 @@ from collections import deque
 def main():
 
     options = "h"
-    long_options = ["help", "install", "data=", "num_threads="]
+    long_options = ["help", "install", "data=", "out=", "num_threads="]
 
     install = False
     data_path = None
+    out_path = None
     num_threads = multiprocessing.cpu_count()
-
 
     try:
         opts, args = getopt.getopt(sys.argv[1:], options, long_options)
@@ -28,6 +28,8 @@ def main():
             install = True
         elif option == "--data":
             data_path = argument
+        elif option == "--out":
+            out_path = argument
         elif option == "--num_threads":
             num_threads = int(argument)
         elif option in ("-h", "--help"):
@@ -35,41 +37,47 @@ def main():
             sys.exit()
 
     if install == True:
-        install_ra()
+        ra_install()
         sys.exit()
+    else:
+        ra_check_install()
 
     if data_path is None:
         error("missing option: --data <file>")
     elif not os.path.isfile(data_path):
         error("non-existent file: --data {}".format(data_path))
 
+    if out_path is None:
+        out_path = os.path.splitext(os.path.basename(data_path))[0] + "_consensus.fasta"
+
     if num_threads < 1:
         error("invalid number of threads: --num_threads {}".format(num_threads))
 
-    ra(data_path, num_threads);
+    ra(data_path, out_path, num_threads);
 
 #*******************************************************************************
 #*******************************************************************************
 
-def install_ra():
-    print("Installing Ra")
+install_dir = os.path.dirname(os.path.realpath(__file__))
 
-    install_dir = os.path.realpath(".")
-    log = open(install_dir + "ra.log", "w")
+def ra_install():
+    print("Installing Ra {")
+
+    log = open(os.path.join(install_dir, "ra_install.log"), "w")
 
     wait(subprocess.Popen(["git", "submodule", "init"], stdout=log, stderr=log))
     wait(subprocess.Popen(["git", "submodule", "update"], stdout=log, stderr=log))
 
-    print("  compiling Minimap ... ", end="", flush=True)
+    print("    compiling Minimap ... ", end="", flush=True)
 
     os.chdir(os.path.join(install_dir, "vendor/minimap"))
     wait(subprocess.Popen("make", stdout=log, stderr=log))
 
     if not os.path.isfile(os.path.join(os.path.realpath("."), "minimap")):
-        error("unable to compile Minimap, check ra.log file.")
+        error("unable to compile Minimap, check ra_install.log file.")
     print("complete!")
 
-    print("  compiling Rala ... ", end="", flush=True)
+    print("    compiling Rala ... ", end="", flush=True)
 
     os.chdir(os.path.join(install_dir, "vendor/rala"))
     wait(subprocess.Popen(["git", "submodule", "init"], stdout=log, stderr=log))
@@ -77,23 +85,40 @@ def install_ra():
     wait(subprocess.Popen("make", stdout=log, stderr=log))
 
     if not os.path.isfile(os.path.join(os.path.realpath("."), "rala")):
-        error("unable to compile Rala, check ra.log file.")
+        error("unable to compile Rala, check ra_install.log file.")
     print("complete!")
 
-    print("  compiling Racon ... ", end="", flush=True)
+    print("    compiling Racon ... ", end="", flush=True)
     os.chdir(os.path.join(install_dir, "vendor/racon"))
     wait(subprocess.Popen(["make", "modules"], stdout=log, stderr=log))
     wait(subprocess.Popen("make", stdout=log, stderr=log))
 
     if not os.path.isfile(os.path.join(os.path.realpath("."), "bin/racon")):
-        error("unable to compile Racon, check ra.log file.")
+        error("unable to compile Racon, check ra_install.log file.")
     print("complete!")
+    print("}")
+
+def ra_check_install():
+    if not os.path.isfile(os.path.join(install_dir, "vendor/minimap/minimap")):
+        error("run install before runnin Ra!")
+    if not os.path.isfile(os.path.join(install_dir, "vendor/rala/rala")):
+        error("run install before runnin Ra!")
+    if not os.path.isfile(os.path.join(install_dir, "vendor/racon/bin/racon")):
+        error("run install before runnin Ra!")
 
 #*******************************************************************************
 #*******************************************************************************
 
-def ra(data_path, num_threads):
-    print("Running Ra with {}".format(data_path))
+def ra(data_path, out_path, num_threads):
+    print("Running Ra on %d threads {" % num_threads)
+    print("    <- input: {}".format(data_path))
+
+    print("    overlap stage (Minimap):")
+    print("    layout stage (Rala):")
+    print("    consensus stage (Racon):")
+
+    print("    -> output: {}".format(out_path))
+    print("}")
 
 #*******************************************************************************
 #*******************************************************************************
